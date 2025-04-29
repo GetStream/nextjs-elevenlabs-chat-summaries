7321d19d46c6be217b4658c96307109fa22ba2b1
Stefan <stefan.blos@gmail.com>
2025-04-29 13:36:36 +0200

    Basic Stream App working

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,80 @@
-import Image from "next/image";
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import { User } from 'stream-chat';
+import { LoadingIndicator } from 'stream-chat-react';
+import MyChat from '@/components/MyChat';
+
+type HomeState = {
+  apiKey: string;
+  user: User;
+  token: string;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [homeState, setHomeState] = useState<HomeState | undefined>();
+  const [error, setError] = useState<string | undefined>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  // TODO: The userId and userName must be set for the code to run. Either dynamically after log-in or hardcoded
+  const userId =
+    'uypg6c2hmxu93shhbdnk8ezy2d343qp4zc5jzmcu9eajmgazdcb7m7a2e4tgprns';
+  const userName = 'Test User';
+
+  const getUserToken = useCallback(async (userId: string, userName: string) => {
+    const response = await fetch('/api/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userId: userId }),
+    });
+
+    const responseBody = await response.json();
+    if (response.status !== 200) {
+      setError(responseBody.message);
+    }
+    const token = responseBody.token;
+
+    if (!token) {
+      setError('No token found');
+    }
+
+    const user: User = {
+      id: userId,
+      name: userName,
+      image: `https://getstream.io/random_png/?id=${userId}&name=${userName}`,
+    };
+
+    const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+    if (apiKey) {
+      setHomeState({ apiKey: apiKey, user: user, token: token });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!userId) {
+      setError('User id is not set.');
+      return;
+    }
+
+    if (!userName) {
+      setError('User name is not set.');
+      return;
+    }
+    getUserToken(userId, userName);
+  }, [userId, userName, getUserToken]);
+
+  if (error) {
+    return (
+      <section className='bg-red-100 text-red-700 font-bold w-screen h-screen flex items-center justify-center'>
+        <p>{error}</p>
+      </section>
+    );
+  }
+
+  if (homeState) {
+    return <MyChat {...homeState} />;
+  } else {
+    <section className='w-screen h-screen flex items-center justify-center'>
+      <LoadingIndicator />
+    </section>;
+  }
 }
---
+++ components/MyChat.tsx
@@ -0,0 +1,52 @@
+import {
+  Chat,
+  Channel,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+  useCreateChatClient,
+  ChannelHeader,
+} from 'stream-chat-react';
+import { ChannelSort, User } from 'stream-chat';
+import 'stream-chat-react/dist/css/v2/index.css';
+import { EmojiPicker } from 'stream-chat-react/emojis';
+
+export default function MyChat({
+  apiKey,
+  user,
+  token,
+}: {
+  apiKey: string;
+  user: User;
+  token: string;
+}) {
+  const chatClient = useCreateChatClient({
+    apiKey: apiKey,
+    tokenOrProvider: token,
+    userData: user,
+  });
+
+  if (!chatClient) {
+    return <div>Error, please try again later.</div>;
+  }
+
+  const filters = { members: { $in: [user.id] } };
+  const sort: ChannelSort = { last_message_at: -1 };
+  const options = { limit: 10 };
+
+  return (
+    <Chat client={chatClient} theme='str-chat__theme-light'>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel EmojiPicker={EmojiPicker}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+}
